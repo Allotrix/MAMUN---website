fd10a1a9ab39dde451a79e6bec00ee8c31efe4b1
Atheeb <atheebvalued@gmail.com>
2024-05-31 18:38:22 +0530

    gradient box and text

--- src/components/GradientBox.jsx
+++ src/components/GradientBox.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const GradientBox = ({tag, title, photo="https://i.postimg.cc/0yqtCSnw/IMG-8571.png"}) => {
     return (
-        <div className="flex min-h-screen items-center justify-center px-12 w-[100%]">
-          <div className="rounded-[12px] p-px bg-gradient-to-b from-mamun-green to-mamun-light-blue ">
+        <div className="flex min-h-min items-center justify-center px-12 w-[100%]">
+          <div className="rounded-[12px] p-px bg-gradient-to-r from-mamun-green to-mamun-light-blue ">
             <div className="rounded-[12px] p-10 bg-[black] text-[white] flex  justify-center items-center ">
 
                 <div className='w-8/12 flex flex-col gap-6'>
 
-                <p className="text-[#CDCDCD] font-bold ">
+                <p className="text-[#CDCDCD] text-left font-bold ">
                    {tag}
                 </p>
-                <h1 className='font-bold text-white text-2xl'>{title}</h1>
+                <h1 className='font-bold text-white text-left text-2xl'>{title}</h1>
 
-                <button className='border-[1px] border-mamun-light-blue bg-transparent px-[60px] py-[2px] rounded-md w-[50%] flex items-center justify-center'>
+                <button className='border-[1px] border-mamun-light-blue bg-transparent px-[60px] py-[2px] rounded-md w-[40%] flex items-center justify-center'>
                     Register
                 </button>
                 </div>
--- src/pages/Committees.jsx
+++ src/pages/Committees.jsx
@@ -3,9 +3,13 @@ import GradientBox from '../components/GradientBox'
 const Committees = () => {
   return (
     <>
-    <main className='bg-allotrix-bg text-allotrix-text relative overflow-hidden'>
-            <section id='committees' className='flex bg-allotrix-bg text-allotrix-text flex-col items-center py-24 text-center gap-10 md:items-start md:text-left md:px-32 md:gap-4 md:pt-48'>
-                <GradientBox/>
+    <main className='relative overflow-hidden'>
+            <section id='committees' className='flex  flex-col text-center gap-8 py-24 mt-24  '>
+                <h1 className='text-[60px] text-[white]'>Committees</h1>
+                <GradientBox tag={"UNITED NATIONS GENERAL ASSEMBLY"} title={"LOREM IPSUM DOLOR AMET SIT, PABLO ESCOBAR"}/>
+                <GradientBox tag={"UNITED NATIONS GENERAL ASSEMBLY"} title={"LOREM IPSUM DOLOR AMET SIT, PABLO ESCOBAR"}/>
+                <GradientBox tag={"UNITED NATIONS GENERAL ASSEMBLY"} title={"LOREM IPSUM DOLOR AMET SIT, PABLO ESCOBAR"}/>
+
             </section>
         </main>
     
