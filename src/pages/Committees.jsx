7906dcd572df37e37ea36140e3e77704c70511a2
Atheeb <atheebvalued@gmail.com>
2024-05-31 18:52:10 +0530

    gradient box and text

--- src/pages/Committees.jsx
+++ src/pages/Committees.jsx
@@ -8,11 +8,15 @@ const Committees = () => {
             <section id='committees' className='flex  flex-col text-center gap-8 py-24 mt-24  '>
                 <h1 className='text-[60px] text-[white]'>Committees</h1>
 
+                <h1 className='gradient-subheading'>Hello world</h1>
+
                 {COMMITTEES.map((element, index)=>(
 
                     <GradientBox key = {index} tag={element.com} title={element.comAgenda} photo={element.comLogo}/>
 
                 ))}
+
+
 
             </section>
         </main>
