9b9e5dc517c578eaff0f974eb7b1e38fe2d13540
Saif <Saifsep17@gmail.com>
2024-05-31 20:00:37 +0530

    Modified UI of checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from "./pages/Checkout";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/checkout' element={<Checkout />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,17 +1,242 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Checkout = () => {
+  const [checkoutForm, setCheckoutForm] = useState({
+    email: "",
+    phoneNo: "",
+    fullName: "",
+    country: "INDIA",
+    addressLine1: "",
+    addressLine2: "",
+    city: "",
+    pinCode: "",
+    state: "",
+  });
+
+  const getPriceForPlan = () => 2500;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const selectedPlan = "Standard";
+
   return (
-    <div>
-      Checkout
-    </div>
-  )
-}
+    <main className="w-full min-h-screen flex flex-col md:flex-row items-center justify-center bg-black text-white">
+      <div className="max-w-full py-6 shadow-lg rounded-lg flex flex-col md:flex-row space-y-6 md:space-x-36">
+        <div className="w-full md:w-1/2">
+          <h2 className="text-2xl font-semibold mb-6">Billing Information</h2>
+          <form
+            onSubmit={handleSubmit}
+            className="font-allotrix-font flex flex-col gap-3 w-[350px]"
+          >
+            <label htmlFor="email" className="flex flex-col gap-1">
+              <p className="text-sm text-white font-extralight">Email ID</p>
+              <input
+                type="email"
+                name="email"
+                value={checkoutForm.email}
+                onChange={(e) =>
+                  setCheckoutForm({
+                    ...checkoutForm,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+                className="px-3 py-[7px] outline-none rounded-md w-full bg-transparent border border-[#c7c6c6] text-md"
+                required
+              />
+            </label>
+            <label htmlFor="phoneNo" className="flex flex-col gap-1">
+              <p className="text-sm text-white font-extralight">Phone Number</p>
+              <input
+                type="tel"
+                name="phoneNo"
+                value={checkoutForm.phoneNo}
+                onChange={(e) =>
+                  setCheckoutForm({
+                    ...checkoutForm,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+                className="px-3 py-[7px] outline-none rounded-md w-full bg-transparent border border-[#c7c6c6] text-md"
+                required
+              />
+            </label>
+            <div className="flex flex-col gap-0 mt-5">
+              <p className="text-md text-white">Billing Address</p>
+
+              <input
+                type="text"
+                placeholder="Full Name"
+                name="fullName"
+                value={checkoutForm.fullName}
+                onChange={(e) =>
+                  setCheckoutForm({
+                    ...checkoutForm,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+                className="px-3 py-[7px] outline-none rounded-md w-full bg-transparent border border-[#c7c6c6]"
+                required
+              />
+              <input
+                type="text"
+                placeholder="Country"
+                name="country"
+                value={checkoutForm.country}
+                disabled
+                className="px-3 py-[7px] cursor-not-allowed outline-none w-full bg-transparent border border-[#c7c6c6]"
+              />
+
+              <input
+                type="text"
+                placeholder="Address Line 1"
+                value={checkoutForm.addressLine1}
+                onChange={(e) =>
+                  setCheckoutForm({
+                    ...checkoutForm,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+                name="addressLine1"
+                className="px-3 py-[7px] w-full outline-none bg-transparent border border-[#c7c6c6]"
+                required
+              />
+              <input
+                type="text"
+                placeholder="Address Line 2"
+                name="addressLine2"
+                value={checkoutForm.addressLine2}
+                onChange={(e) =>
+                  setCheckoutForm({
+                    ...checkoutForm,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+                className="px-3 py-[7px] w-full outline-none bg-transparent border border-[#c7c6c6]"
+                required
+              />
+              <div className="flex gap-0">
+                <input
+                  type="text"
+                  placeholder="City"
+                  name="city"
+                  value={checkoutForm.city}
+                  onChange={(e) =>
+                    setCheckoutForm({
+                      ...checkoutForm,
+                      [e.target.name]: e.target.value,
+                    })
+                  }
+                  className="px-3 py-[7px] w-[50%] bg-transparent outline-none border border-[#c7c6c6]"
+                  required
+                />
+                <input
+                  type="text"
+                  placeholder="Pincode"
+                  name="pinCode"
+                  value={checkoutForm.pinCode}
+                  onChange={(e) =>
+                    setCheckoutForm({
+                      ...checkoutForm,
+                      [e.target.name]: e.target.value,
+                    })
+                  }
+                  className="px-3 py-[7px] w-[50%] bg-transparent outline-none border border-[#c7c6c6]"
+                  required
+                />
+              </div>
+              <input
+                type="text"
+                placeholder="State"
+                name="state"
+                value={checkoutForm.state}
+                onChange={(e) =>
+                  setCheckoutForm({
+                    ...checkoutForm,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+                className="px-3 py-[7px] rounded-b-md w-full outline-none bg-transparent border border-[#c7c6c6]"
+                required
+              />
+            </div>
+          </form>
+        </div>
+
+        <div className="w-full md:w-1/2 flex flex-col items-center">
+          <div className="top-4 right-4 text-sm text-white ml-auto font-bold mb-2">
+            Powered by <span className="text-orange-400">Astrapí Money</span>
+          </div>
+          <div className="w-full max-w-md p-8 mx-auto bg-[#33363F] shadow-lg rounded-lg text-left">
+            <div className="text-center text-xl font-bold mb-4">
+              Pay Allotrix
+            </div>
+            <div className="text-3xl font-bold text-center mb-6">
+              ₹{getPriceForPlan()}.00
+            </div>
+            <div className="space-y-2 text-gray-400">
+              <div className="flex justify-between">
+                <span>Delegate Fee</span>
+                <span>₹ 2,050</span>
+              </div>
+              <hr />
+              <div className="flex justify-between border-t border-gray-700 pt-2 mt-2">
+                <span className="text-white">Subtotal</span>
+                <span className="text-white">₹ 2,050</span>
+              </div>
+              <div className="flex justify-between">
+                <span>Platform Fee</span>
+                <span>₹ 20</span>
+              </div>
+              <div className="flex justify-between">
+                <span>CGST</span>
+                <span>₹ 0</span>
+              </div>
+              <div className="flex justify-between">
+                <span>Convenience Fee</span>
+                <span>₹ 0</span>
+              </div>
+              <hr />
+              <div className="flex justify-between border-t border-gray-700 pt-2 mt-2 font-bold">
+                <span className="text-white">Total</span>
+                <span className="text-white">₹ 2,500</span>
+              </div>
+              <div className="text-center">
+                <button className="w-1/2 mt-4 py-2 bg-[#02E29E] text-black rounded font-bold hover:bg-green-600">
+                  Check out
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <footer className="w-full max-w-md px-8 py-6 mx-auto shadow-lg rounded-lg mt-6 text-center text-sm text-gray-400 align-center">
+            <div className="flex justify-center space-x-4 mb-4">
+              <a href="#" className="hover:underline ">
+                Privacy Policy
+              </a>
+              <a href="#" className="hover:underline ">
+                Get Allotrix
+              </a>
+              <a href="#" className="hover:underline">
+                Get Astrapí Money
+              </a>
+            </div>
+            <p>
+              Note: Astrapí Money belongs to the same domain as Allotrix.
+              Allotrix uses Astrapí Money's plugin to verify payments. Review
+              Astrapí Money's Privacy Policy | Terms and conditions to know
+              more.
+            </p>
+          </footer>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default Checkout;
 
-
-{/* <main className='bg-white text-[#303030] relative overflow-hidden z-10 flex flex-col md:flex-row justify-center gap-14 md:gap-[unset]'>
+{
+  /* <main className='bg-white text-[#303030] relative overflow-hidden z-10 flex flex-col md:flex-row justify-center gap-14 md:gap-[unset]'>
             <section className='md:w-[50%] flex md:justify-end justify-center md:pr-24 pt-20'>
                 <div className='flex flex-col gap-10 md:w-[400px] w-[350px]'>
                     <Link to={'/getallotrix/download/payment'} className='text-2xl'>
@@ -132,4 +357,5 @@ export default Checkout;
                 </a>
             </aside>
         </main>
-        </> */}
+        </> */
+}
