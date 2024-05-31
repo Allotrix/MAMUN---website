d14faf9fa1a014e3cde4cd6514cfad308d3cf3fa
Saif <Saifsep17@gmail.com>
2024-05-31 21:51:03 +0530

    Made UI changes, like colors and font to the Checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Committees from './pages/Committees';
-import Checkout from './pages/Checkout';
-import Navbar from './components/Navbar';
-import MobileNavbar from './components/MobileNavbar';
-import Home from './pages/Home';
-import Gallery from './pages/Gallery';
-import Footer from './components/Footer';
-import Cursor from './components/Cursor';
-import Secretariat from './pages/Secretariat';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Committees from "./pages/Committees";
+import Checkout from "./pages/Checkout";
+import Navbar from "./components/Navbar";
+import MobileNavbar from "./components/MobileNavbar";
+import Home from "./pages/Home";
+import Gallery from "./pages/Gallery";
+import Footer from "./components/Footer";
+import Cursor from "./components/Cursor";
+import Secretariat from "./pages/Secretariat";
 
 const App = () => {
-
   const [openNav, setOpenNav] = useState(false);
 
   return (
     <Router>
-          <Cursor/>
+      <Cursor />
 
       <Navbar openNav={openNav} setOpenNav={setOpenNav} />
       <MobileNavbar openNav={openNav} setOpenNav={setOpenNav} />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/checkout' element={<Checkout />} />
-        <Route path='/secretariat' element={<Secretariat />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/secretariat" element={<Secretariat />} />
 
-        <Route path='/committees' element={<Committees />} />
-        <Route path='/gallery' element={<Gallery />} />
+        <Route path="/committees" element={<Committees />} />
+        <Route path="/gallery" element={<Gallery />} />
       </Routes>
       <Footer />
     </Router>
