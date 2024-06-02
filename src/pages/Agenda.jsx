1268ce8593e487cd5934d9f9ba4e52df3e1a2dde
Atheeb <atheebvalued@gmail.com>
2024-06-02 17:41:30 +0530

    fixed committe/agenda/eb

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -6,23 +6,27 @@ import StickyScroll from '../components/StickyScroll';
 const Agenda = () => {
 
     const [committee, setCommittee] = useState({});
+    const [comIndex, setComIndex] = useState(0)
     const [showAbout, setShowAbout] = useState(false);
+    const [showEB, setShowEB] = useState(false);
 
+    console.log(COMMITTEES["board"])
     const params = useParams();
     const { comName } = params;
 
     useEffect(() => {
         const fetchCommittee = () => {
-            COMMITTEES.forEach((com) => {
-                if(com.comAbb.toLowerCase() === comName) {
-                    setCommittee(com);
-                    return;
-                }
-            })
-        }
-
+          for (let i = 0; i < COMMITTEES.length; i++) {
+            if (COMMITTEES[i].comAbb.toLowerCase() === comName.toLowerCase()) {
+              setCommittee(COMMITTEES[i]);
+              setComIndex(i);
+              break;
+            }
+          }
+        };
+    
         fetchCommittee();
-    }, []);
+      }, [comName]);
 
     return (
         <main className='font-mamun-font-secondary text-[white]'>
@@ -37,10 +41,10 @@ const Agenda = () => {
                     <a href={committee.bgGuide} className='text-xl text-[black] bg-mamun-green rounded-md px-20 py-3 min-w-[260px]'>
                         BG Guide
                     </a>
-                    <button className='text-xl text-[black] bg-mamun-green rounded-md px-20 py-3 min-w-[260px]'>
+                    <button  onClick={() => {setShowEB(true); setShowAbout(false)}} className='text-xl text-[black] bg-mamun-green rounded-md px-20 py-3 min-w-[260px]'>
                         EB
                     </button>
-                    <button onClick={() => setShowAbout(true)} className='text-xl text-[black] bg-[#79F3CE] rounded-md px-20 py-3 min-w-[260px]'>
+                    <button onClick={() => {setShowAbout(true); setShowEB(false)}} className='text-xl text-[black] bg-[#79F3CE] rounded-md px-20 py-3 min-w-[260px]'>
                         About
                     </button>
                 </aside>
@@ -53,9 +57,9 @@ const Agenda = () => {
                     {committee.about}
                 </p>
             </section>
-            {/* <section className={`my-14 w-full px-4 mx-auto`}>
-                <StickyScroll OBJECT={committee.board} />
-            </section> */}
+            <section className={`my-14 w-full px-4 mx-auto ${showEB ? "block": "hidden"}`}>
+                <StickyScroll OBJECT={COMMITTEES[comIndex].board} />
+            </section> 
         </main>
     )
 }
