cc0d6c947b4663c716ecd5a00dd8275ad22e59d2
Atheeb <atheebvalued@gmail.com>
2024-06-06 08:39:14 +0530

    added content

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -26,7 +26,8 @@ export const COMMITTEES = [
         name: "Harirropan Mathan",
         designation: "Director",
         description:
-          "From being a dreaded delegate of USA to dominating any and all committees, Nithin is a fearsome delegate among the MUN circuit, being no stranger to Policy based committees. His fame doesn’t stop there, organising several successful MUN conferences, and participating in over 30 conferences himself. He is also considered the epitome of 'Veni, Vidi, Vici.' by a few. From turning committees into chaos and mayhem, Nithin promises to turn this into a smooth and successful conference as the chief advisor",
+              `In the realm of Model United Nations, Hariroopan Mathan stands as a respected figure, having actively participated in 36 conferences over the span of seven years. Throughout his MUN journey, Hariroopan has not only honed critical skills such as public speaking, negotiation, and critical thinking but has also cultivated meaningful friendships with peers from diverse backgrounds, fostering a sense of camaraderie and mutual respect. Beyond the confines of the MUN circuit, Hariroopan finds solace and inspiration in the world of cinema, particularly in the works of renowned director Martin Scorsese, such as "Goodfellas" and "Casino." These films, with their intricate narratives and compelling characters, offer Hariroopan both entertainment and intellectual stimulation. Through his passion for MUN and his appreciation for cinema, Hariroopan continues to evolve personally and intellectually, enriching his perspective on the world around him.
+              `,
         img: "https://i.postimg.cc/fycyG9fZ/Whats-App-Image-2024-06-05-at-9-22-29-PM.webp",
       },
     ],
@@ -75,21 +76,29 @@ export const COMMITTEES = [
       {
         name: "Ravivatsan",
         designation: "Speaker",
-        description: "i am a pretty girl",
+        description: `-If you have ever been to an Indian committee in Southern India, you must have already met Ravivatsan or infamously heard of his intimidating  presence owing to his  consistent wins across numerous conferences. 
+
+        He is an engineer by education but a keen observer of Indian polity, constitutional law, and policy-making by passion. Having been in the MUN circuit since 2018, he has been debating for so long that Everybody eagerly wants to know what would happen if he didn't win in a debate; unfortunately we guess  we'll never know.
+        
+        Let it be " Ontology", "Oscars", or "the Octagon" - you can have a conversation with him about anything and everything. As an executive board member he expects participants to come with  extensive research, intensive preparation and a  tinge of self-confidence. While walking in for the conference he recommends you to remember something that he believes and lives by : " If the world refuses to shake hands with you, shake the world
+        `,
         img: "https://i.postimg.cc/25qyfk6M/Ravivatsan.webp        ",
       },
       {
         name: "Sujith Kumar",
         designation: "Deputy Speaker",
-        description:
-          'a third-year Computer Science student at SRM Institute of Science and Technology is an avid public speaker with an appetite for Indian Polity. While he admires extensive research, he understands that the ability to remain humble separates a good delegate from a great delegate, and firmly believes in the revered Tamil couplet Thirukkural, which states "பிறப்பொக்கும் எல்லா உயிர்க்கும் சிறப்பொவ்வா செய்தொழில் வேற்றுமை யான்."',
+        description:`
+        Sujith Kumar, a third-year Computer Science student at SRM Institute of Science and Technology is an avid public speaker with an appetite for Indian Polity. While he admires extensive research, he understands that the ability to remain humble separates a good delegate from a great delegate, and firmly believes in the revered Tamil couplet Thirukkural, which states
+"பிறப்பொக்கும் எல்லா உயிர்க்கும் சிறப்பொவ்வா
+செய்தொழில் வேற்றுமை யான்."
+`,
         img: "https://i.postimg.cc/zXSRdGbk/Sujith.webp        ",
       },
       {
         name: "Ashwath Mahesh",
         designation: "Director",
         description: "left is never right",
-        img: "https://i.postimg.cc/rpz0FGhh/Aswath.webp        ",
+        img: "https://i.postimg.cc/YCdS4Dh1/Aswath.jpg",
       },
     ],
   },
@@ -112,8 +121,7 @@ export const COMMITTEES = [
       {
         name: "Fazil Razak",
         designation: "Co Chairperson",
-        description:
-          "Hey everyone! I'm Akarsh Jaykumar. When I'm not binge-watching comedy classics like ‘Superbad and 21 Jump Street,’ I often listen to music from smooth R&B to rap and jazz. I also enjoy watching football, being a Manchester United fan (it's painful)",
+        description: `Ayush has been doing Model UN for a while and is a familiar face across the length and breadth of the country. He has also chaired internationally, notably at Oxford University and the National University of Singapore.`,
         img: "https://i.postimg.cc/hPhpFvWy/DSC-0933.jpg",
       },
       {
@@ -121,7 +129,7 @@ export const COMMITTEES = [
         designation: "Vice Chairperson",
         description:
           "From being a dreaded delegate of USA to dominating any and all committees, Nithin is a fearsome delegate among the MUN circuit, being no stranger to Policy based committees. His fame doesn’t stop there, organising several successful MUN conferences, and participating in over 30 conferences himself. He is also considered the epitome of 'Veni, Vidi, Vici.' by a few. From turning committees into chaos and mayhem, Nithin promises to turn this into a smooth and successful conference as the chief advisor",
-        img: "https://i.postimg.cc/QtTFc8Z1/Nithin.webp        ",
+        img: "https://i.postimg.cc/4NwJw0MW/Ashwamedh.jpg        ",
       },
     ],
   },
@@ -144,8 +152,7 @@ export const COMMITTEES = [
       {
         name: "TCA Varshini",
         designation: "Co Chairpersonperson",
-        description:`TCA Varshini is a 4th- year law student at VIT Chennai who loves socializing and meeting new people. With a MUN career consisting purely of Best Dels and High Coms, she is an avid debater who enjoys delegating not just for the drama in committee but also because it's a great way of exploring international law, a subject she is passionate about. She has also been on the organising side of MUNs and served as the Deputy Secretary General of VITCMUN'24. In her leisure time, you'd find her re-watching Sherlock, baking or sketching. As someone who once promised to never step into humanitarian committees but eventually ended up specializing in them, she assures you that you won't be disappointed being a part of SOCHUM at this edition of MAMUN.
-        `,
+        description:`TCA Varshini is a 4th- year law student at VIT Chennai who loves socializing and meeting new people. With a MUN career consisting purely of Best Dels and High Coms, she is an avid debater who enjoys delegating not just for the drama in committee but also because it's a great way of exploring international law, a subject she is passionate about. She has also been on the organising side of MUNs and served as the Deputy Secretary General of VITCMUN'24. In her leisure time, you'd find her re-watching Sherlock, baking or sketching. As someone who once promised to never step into humanitarian committees but eventually ended up specializing in them, she assures you that you won't be disappointed being a part of SOCHUM at this edition of MAMUN.`,
         img: "https://i.postimg.cc/0j7J18tP/Varshini.webp",
       },
       {
@@ -167,19 +174,20 @@ export const COMMITTEES = [
     about:
       "Lorem Ipsum Dolor sir amet agenda of the committee goes hereLorem Ipsum Dolor sir amet agenda of the committee goes hereLorem Ipsum Dolor sir amet agenda of the committee goes hereLorem Ipsum Dolor sir amet agenda of the committee goes hereLorem Ipsum Dolor sir amet agenda of the committee goes here",
     board: [
+      
+      {
+        name: "Prabhu Shankar",
+        designation: "Director",
+        description:`Prabhu is a 4th year law student from VIT Chennai. His love for travel and adventure gave him the chance to flaunt his skills as a photographer. This charming photographer is an avid explorer and storyteller who captures the beauty and diversity of the world through their lens. Live has been a rollercoaster in his world of photography, and his awards in almost all MUNS he participated in, reflect this. He was awarded as the winner of the Nikon Shutterbug Contest Season 3 for capturing "the life outdoors in Chennai," and to his joy, he bagged a worthy camera. He is the person one should look forward to for picture-perfect portraits or stills of heated debates.
+        `,
+                img: "https://i.postimg.cc/fRrMQ9MX/Prabhu-Shankar.jpg                ",
+      },
       {
         name: "Abdul Raziq",
         designation: "Secretary",
         description:
           "Introducing Tanishq Dilip an adept negotiator and master of business deals who thrives in the high-stakes world of entrepreneurship and business. His keen interest in these fields makes him an invaluable asset to any discussion table, and his strategic thinking ensures that no detail goes unnoticed. Tanishq approaches every challenge with unmatched confidence and precision. He lives by the words, 'I don't have dreams, I have goals,' reflecting his commitment to setting ambitious targets and consistently exceeding them. His ability to read the room and anticipate every move is unparalleled, allowing him to navigate complex negotiations and launch groundbreaking startups with ease. In the realm of Model United Nations, Tanishq Dilip stands out as a formidable delegate who combines his love for business with his sharp negotiation skills. His persuasive arguments and deft handling of entrepreneurial policies showcase his deep understanding and commitment to creating meaningful solutions.",
         img: "https://i.postimg.cc/SKmSswGP/DSC-0909.jpg",
-      },
-      {
-        name: "Prabhu Shankar",
-        designation: "Director",
-        description:
-          "Hey everyone! I'm Akarsh Jaykumar. When I'm not binge-watching comedy classics like ‘Superbad and 21 Jump Street,’ I often listen to music from smooth R&B to rap and jazz. I also enjoy watching football, being a Manchester United fan (it's painful)",
-        img: "https://i.postimg.cc/hPhpFvWy/DSC-0933.jpg",
       },
     ],
   },
@@ -195,16 +203,18 @@ export const COMMITTEES = [
       {
         name: "Sriyaansh Mohanty",
         designation: "Secretary",
-        description:
-          "Introducing Tanishq Dilip an adept negotiator and master of business deals who thrives in the high-stakes world of entrepreneurship and business. His keen interest in these fields makes him an invaluable asset to any discussion table, and his strategic thinking ensures that no detail goes unnoticed. Tanishq approaches every challenge with unmatched confidence and precision. He lives by the words, 'I don't have dreams, I have goals,' reflecting his commitment to setting ambitious targets and consistently exceeding them. His ability to read the room and anticipate every move is unparalleled, allowing him to navigate complex negotiations and launch groundbreaking startups with ease. In the realm of Model United Nations, Tanishq Dilip stands out as a formidable delegate who combines his love for business with his sharp negotiation skills. His persuasive arguments and deft handling of entrepreneurial policies showcase his deep understanding and commitment to creating meaningful solutions.",
-        img: "https://i.postimg.cc/SKmSswGP/DSC-0909.jpg",
+        description:`A guy of many hats, Sriyansh has never shied away from trying new things. Currently pursuing a PhD in the domain of National Security and Strategic Studies, he brings a wealth of knowledge to the positions he serves at conferences. Sriyansh is frequently referred to be a jack of all trades, mostly due to his versatility in doing different kinds of committees. A name that is well-known for questioning the order, and bringing the best out of people, he looks forward to having a great time in the magnificent city of Chennai.
+        `,
+                img: "https://i.postimg.cc/hGQcDtGY/Sriyansh.jpg                ",
       },
       {
         name: "Subhasri ",
         designation: "Director",
-        description:
-          "Hey everyone! I'm Akarsh Jaykumar. When I'm not binge-watching comedy classics like ‘Superbad and 21 Jump Street,’ I often listen to music from smooth R&B to rap and jazz. I also enjoy watching football, being a Manchester United fan (it's painful)",
-        img: "https://i.postimg.cc/hPhpFvWy/DSC-0933.jpg",
+        description:`Subhasri Manigandan started doing MUNs from her first year and from her very first MUN, she realized her pen was stronger than her spoken word and pledged her allegiance to the International Press. A firm believer of learning and teaching, her penchant for exploring the world of MUNs stemmed from her innate desire to observe, learn and question the world around her.
+
+        Apart from being a part of 40+ MUNs as a reporter and EB member, she served as the Secretary General of YLGC 2024. Today, you might find her analysing conspiracy theories but tomorrow she may binge watch a couple seasons of a show or learn a new language. With her motto, “To learn to live and live to learn”, she aims to conquer her dreams.
+        `,
+        img: "https://i.postimg.cc/SxsmPrbk/Subhasri.jpg        ",
       },
     ],
   },
@@ -395,8 +405,7 @@ export const CHAIRS = [
   {
     name: "Ayush",
     designation: "Co Chairperson | SPECPOL ",
-    description:
-      "Introducing Tanishq Dilip an adept negotiator and master of business deals who thrives in the high-stakes world of entrepreneurship and business. His keen interest in these fields makes him an invaluable asset to any discussion table, and his strategic thinking ensures that no detail goes unnoticed. Tanishq approaches every challenge with unmatched confidence and precision. He lives by the words, 'I don't have dreams, I have goals,' reflecting his commitment to setting ambitious targets and consistently exceeding them. His ability to read the room and anticipate every move is unparalleled, allowing him to navigate complex negotiations and launch groundbreaking startups with ease. In the realm of Model United Nations, Tanishq Dilip stands out as a formidable delegate who combines his love for business with his sharp negotiation skills. His persuasive arguments and deft handling of entrepreneurial policies showcase his deep understanding and commitment to creating meaningful solutions.",
+    description:`Ayush has been doing Model UN for a while and is a familiar face across the length and breadth of the country. He has also chaired internationally, notably at Oxford University and the National University of Singapore.`,
     img: "https://i.postimg.cc/x8DqpRHt/Whats-App-Image-2024-06-04-at-11-59-18-PM.webp",
   },
   {
@@ -418,24 +427,27 @@ export const CHAIRS = [
     name: "Shankar",
     designation: "Head Chairperson | SOCHUM",
     img: "https://i.postimg.cc/3JHvCvRR/Shankar.webp",
-    description:
-      "Lorem Ipusum Dolor Sit amet Lorem Ipusum Dolor Sit ametLorem Ipusum Dolor Sit amet Lorem Ipusum Dolor Sit ametLorem Ipusum Dolor Sit amet Lorem Ipusum Dolor Sit ametLorem Ipusum Dolor Sit amet Lorem Ipusum Dolor Sit ametLorem Ipusum Dolor Sit amet Lorem Ipusum Dolor Sit ametLorem Ipusum Dolor Sit amet Lorem Ipusum Dolor Sit ametLorem Ipusum Dolor Sit amet Lorem Ipusum Dolor Sit amet",
+    description:`Shankar is an experienced Model United Nations (MUN) participant, specializing in human rights councils and policy committees. His passion for international law is evident, especially in his fondness for Malcolm Shaw's writings. Shankar is also known for his spontaneous impromptu trips. Additionally, Shankar is keen on nutrition, anthropology, and Stoicism, showcasing his holistic approach to well-being, curiosity about human societies, and a calm, resilient attitude towards life's challenges. His presence on the executive board ensures a well-rounded, insightful, and enriching experience for all delegates participating in SOCHUM at MAMUN    `,
+  },
+  {
+    name: "Ravivatsan",
+    designation: "Speaker | Lok Sabha",
+    description: `If you have ever been to an Indian committee in Southern India, you must have already met Ravivatsan or infamously heard of his intimidating  presence owing to his  consistent wins across numerous conferences. 
+
+    He is an engineer by education but a keen observer of Indian polity, constitutional law, and policy-making by passion. Having been in the MUN circuit since 2018, he has been debating for so long that Everybody eagerly wants to know what would happen if he didn't win in a debate; unfortunately we guess  we'll never know.
+    
+    Let it be " Ontology", "Oscars", or "the Octagon" - you can have a conversation with him about anything and everything. As an executive board member he expects participants to come with  extensive research, intensive preparation and a  tinge of self-confidence. While walking in for the conference he recommends you to remember something that he believes and lives by : " If the world refuses to shake hands with you, shake the world
+    
+    `,
+    img: "https://i.postimg.cc/25qyfk6M/Ravivatsan.webp",
   },
   {
     name: "Lokesh Narayan",
     designation: "Co Chairperson | DISEC",
     img: "https://i.postimg.cc/mDR7tBpb/Screenshot-2024-06-05-at-10-09-28-PM.png",
     description:
-      "Lorem Ipusum Dolor Sit amet Lorem Ipusum Dolor Sit ametLorem Ipusum Dolor Sit amet Lorem Ipusum Dolor Sit ametLorem Ipusum Dolor Sit amet Lorem Ipusum Dolor Sit ametLorem Ipusum Dolor Sit amet Lorem Ipusum Dolor Sit ametLorem Ipusum Dolor Sit amet Lorem Ipusum Dolor Sit ametLorem Ipusum Dolor Sit amet Lorem Ipusum Dolor Sit ametLorem Ipusum Dolor Sit amet Lorem Ipusum Dolor Sit amet",
-  },
-  
-  
-  {
-    name: "Ravivatsan",
-    designation: "Speaker | SURPRISE",
-    description: "i am a pretty girl",
-    img: "https://i.postimg.cc/25qyfk6M/Ravivatsan.webp",
-  },
+`With a knack for quirky remarks and witty jokes, Lokesh has been associated with the Chennai MUN circuit since his school days. As someone who doesn’t get caught in the spin of mundane messes, he possesses an ability to stay calm at the most unsettled times, resolving conflicts is his stronghold. 
+`  },
   
   
 ];
